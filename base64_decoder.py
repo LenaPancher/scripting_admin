1d9fa06df9ebc5d47ddc5b803633fdfadc9179ff
Moiseng <48313624+Moiseng@users.noreply.github.com>
2022-06-29 15:53:50 +0200

    feat: add log to base64 decoder
    Add logging system on each method to have a trace of each operation

--- base64_decoder.py
+++ base64_decoder.py
@@ -1,7 +1,11 @@
 import base64_encoder_v2 as encoder
 import re
-import sys
-sys.setrecursionlimit(1500)
+import logging
+
+import logging_conf
+from logging_conf import *
+"""import sys
+sys.setrecursionlimit(1500)"""
 
 def decode_starter():
     """
@@ -16,36 +20,20 @@ def decode_starter():
         if matches:
             # second step
             string_formatted = remove_base64_last_characters(input_value)
-            print("step 1")
-            print(string_formatted)
             # third step
             array_base64_list_string = base64_string_to_array(string_formatted)
-            print("step 2")
-            print(array_base64_list_string)
             # fourth step
             array_base64_int = convert_base_array_to_base64_int(array_base64_list_string)
-            print("step 3")
-            print(array_base64_int)
             # fifth step
             array_binary = convert_list_of_int_to_binary(array_base64_int)
-            print("step 4")
-            print(array_binary)
             #sixth step
             array_binary_with_zero = add_zero_before_or_after_value(array_binary)
-            print("step 5")
-            print(array_binary_with_zero)
             # seventh step
             binary_join_to_single_string = convert_binary_to_ascii(array_binary_with_zero)
-            print("step 6")
-            print(binary_join_to_single_string)
             # height step
             val = remove_value_len_less_than_height(binary_join_to_single_string)
-            print("step 7")
-            print(val)
             # ninth step
             value_without_zero_at_beggining = remove_zero_at_beggining(val)
-            print("step 8")
-            print(value_without_zero_at_beggining)
             # tenth
             return convert_ascii_to_string(value_without_zero_at_beggining)
         else:
@@ -62,6 +50,7 @@ def check_if_value_is_base64_encoded(input_value):
     """
     expression = "^([A-Za-z0-9+/]{4})*([A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{2}==)?$"
     matches = re.match(expression, input_value)
+    logging.info(matches)
     return matches
 
 
@@ -72,7 +61,9 @@ def remove_base64_last_characters(base64_value):
     Args:
         base64_value: the given base64 string
     """
-    return re.sub(r"[^a-zA-Z0-9]", "", base64_value)
+    result = re.sub(r"[^a-zA-Z0-9]", "", base64_value)
+    logging.info(result)
+    return result
 
 
 def base64_string_to_array(value: str):
@@ -83,7 +74,9 @@ def base64_string_to_array(value: str):
     Args:
         value: base64 single line string
     """
-    return list(value)
+    list_result = list(value)
+    logging.info(list_result)
+    return list_result
 
 
 def convert_base_array_to_base64_int(list_string: list):
@@ -97,6 +90,8 @@ def convert_base_array_to_base64_int(list_string: list):
     base64_list_int = []
     for i in list_string:
         base64_list_int.append(base64_to_list.index(i))
+    logging.info("converting base64 list of string to list of integer")
+    logging.info(base64_list_int)
     return base64_list_int
 
 
@@ -109,6 +104,7 @@ def convert_list_of_int_to_binary(list_int):
     binary_array = []
     for i in list_int:
         binary_array.append(bin(i).replace("0b", ""))
+    logging.info(binary_array)
     return binary_array
 
 
@@ -118,7 +114,9 @@ def convert_binary_to_ascii(binary_list):
     Args:
         binary_list: Binary list
     """
-    return encoder.join_elements_to_string(binary_list)
+    final_string = encoder.join_elements_to_string(binary_list)
+    logging.info(final_string)
+    return final_string
     
 
 def cut_binary_string(binary_string: str, by):
@@ -128,7 +126,9 @@ def cut_binary_string(binary_string: str, by):
         binary_string: Binary string
         by: ( int ) the len of each cut
     """
-    return encoder.cut_string_array_6_length(binary_string, by)
+    string_cuted = encoder.cut_string_array_6_length(binary_string, by)
+    logging.info(string_cuted)
+    return string_cuted
 
 
 def add_zero_before_or_after_value(binary_int_list):
@@ -147,6 +147,7 @@ def add_zero_before_or_after_value(binary_int_list):
             while len(v) < 6:
                 v = v + "0"
             array.append(v)
+    logging.info(array)
     return array
 
 
@@ -161,6 +162,7 @@ def remove_value_len_less_than_height(value):
     for v in string_val:
         if len(v) == 8:
             new_array.append(v)
+    logging.info(new_array)
     return new_array
     
 
@@ -175,11 +177,11 @@ def remove_zero_at_beggining(value):
     new_array_of_number = []
     for v in value:
         new_array.append(v[1:])
+    logging.info(new_array)
 
-    print(new_array)
     for i in new_array:
         new_array_of_number.append(int(i, 2))
-
+    logging.info(new_array_of_number)
     return new_array_of_number
 
 
@@ -194,9 +196,9 @@ def convert_ascii_to_string(val):
     value_converted = []
     for v in val:
         value_converted.append(chr(v))
-    print("result final : ")
-    print("".join(value_converted))
-    return "".join(value_converted)
+    result = "".join(value_converted)
+    logging.info(result)
+    return result
     
 
 if __name__ =="__main__":
