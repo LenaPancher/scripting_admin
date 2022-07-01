602486af32e27a50b7d53d4f15deeff6b045b61c
lucas <lucasgauvain@gmail.com>
2022-07-01 13:17:17 +0200

    administration.py and service done

--- supervision/administration.py
+++ supervision/administration.py
@@ -3,7 +3,6 @@
 import psutil
 import argparse
 from time import sleep
-from datetime import datetime
 import json
 from influxdb import InfluxDBClient
 
@@ -25,45 +24,37 @@ def main():
 def parser():
     args_parser = argparse.ArgumentParser('show system information')
 
-    args_parser.add_argument("-i", "--interval", help="choose interval output in second", type=int,
-                             action="store")
-    args_parser.add_argument('--host', type=str, required=False,
-                             default='localhost',
-                             help='hostname of InfluxDB http API')
-    args_parser.add_argument('--port', type=int, required=False, default=8086,
-                             help='port of InfluxDB http API')
+    args_parser.add_argument("-i", "--interval", help="choose interval output in second", type=int, action="store")
+    args_parser.add_argument('-ho', '--host', type=str, required=False, help='hostname of InfluxDB http API', action="store")
+    args_parser.add_argument('-p', '--port', type=int, required=False, help='port of InfluxDB http API', action="store")
 
     args = args_parser.parse_args()
 
-    show_output(args.interval)
+    show_output(args.interval, args.host, args.port)
 
 
-def show_output(interval):
-    save_to_influxdb()
-    return
+def show_output(interval, host, port):
     interval = interval if interval is not None else 5
-    console_print()
+    logging()
     while True:
         sleep(interval)
-        console_print()
+        logging()
+        save_to_influxdb(host, port)
 
 
-def save_to_influxdb():
+def save_to_influxdb(host, port):
     """Instantiate a connection to the InfluxDB."""
     user = 'root'
     password = 'root'
     dbname = 'psutils'
-    dbuser = 'smly'
-    host = '192.168.10.110'
-    port = '8086'
-    dbuser_password = 'my_secret_password'
-    query = 'select Float_value from cpu_load_short;'
+    host = host if host is not None else 'localhost'
+    port = port if port is not None else 8086
+
     metrics: Metrics = get_metrics()
     json_metrics = json.dumps(metrics.__dict__)
     json_payload = []
     object_payload = json.loads(
-        '{ "measurement": "psutils", "tags": { "host": "psutils" }, '
-        '"time": "' + str(datetime.now()) + '", "fields": ' + json_metrics + ' }')
+        '{ "measurement": "psutils", "tags": { "host": "psutils" }, "fields": ' + json_metrics + ' }')
     json_payload.append(object_payload)
 
     client = InfluxDBClient(host, port, user, password, dbname)
@@ -74,17 +65,14 @@ def save_to_influxdb():
     print("Create a retention policy")
     client.create_retention_policy('awesome_policy', '3d', 3, default=True)
 
-    print("Switch user: " + dbuser)
-    client.switch_user(dbuser, dbuser_password)
-
     print("Write points: {0}".format(json_payload))
     client.write_points(json_payload)
 
 
-def console_print():
+def logging():
     metrics: Metrics = get_metrics()
-    print("")
-    print("# SYSTEM METRICS ")
+    mylogs.info("")
+    mylogs.info("# SYSTEM METRICS ")
     mylogs.info(f"# Used Memory :  {metrics.memory_usage} % ")
     mylogs.info(f"# Network Usage :  {metrics.bytes_sent} ")
     mylogs.info(f"# CPU Usage :  {metrics.cpu_usage}  %")
@@ -104,7 +92,7 @@ def get_metrics():
 
     megabytes_sent = f"{key} : {net[key].bytes_sent / float(1 << 20):,.0f} MB"
 
-    return Metrics(memory.percent, megabytes_sent, sensors.percent, cpu)
+    return Metrics(float(memory.percent), megabytes_sent, float(sensors.percent), float(cpu))
 
 
 if __name__ == '__main__':
